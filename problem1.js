39170b6888c15011d961aa926563e2d37c53f427
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-05 08:04:00 +0000

    Information of car ID 33

---
+++ problem1.js
@@ -0,0 +1,12 @@
+let carInfo= function (inventory){
+    for(let i=0;i<inventory.length;i++)
+    {
+        if(inventory[i]['id'] === 33)
+        {
+            console.log(`car 33 is a  ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`)
+        }
+    }
+}
+module.exports = carInfo;
+
+
